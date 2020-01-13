d77a5f01cc5e430e7e5b22e6e15da5df4975630e
Robervald da Silva Sidrome <rsidrome@gmail.com>
2020-01-13 09:58:59 -0400

    projeto para aprentação de dados dashboad super ensino

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+const Menu = [
+  {
+    label: "Home",
+    pathname: "/"
+  },
+  {
+    label: "Dashboard",
+    pathname: "/dashboard"
+  },
+  {
+    label: "",
+    pathname: "/signup"
+  },
+  {
+    label: "",
+    pathname: "/wizard"
+  },
+  {
+    label: "",
+    pathname: "/cards"
+  },
+  {
+    label: "",
+    pathname: "https://github.com/alexanmtz/material-sense",
+    external: true
+  }
+
+];
+
+export default Menu;
